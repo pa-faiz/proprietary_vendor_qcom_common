cccddd18524ef152b01c0f3fe550741a1b6d91c0
Fazil Sheik <fazilsheik@aospa.co>
2025-08-05 17:53:15 +0530

    vendor: display: Import 6.6 component
    From LA.VENDOR.15.4.0.r1-15000-pakala.0-1
    
    Change-Id: Id4b2c1eae2c851e773f572234bc259b9d2f113a3
    Signed-off-by: Fazil Sheik <fazilsheik@aospa.co>

---
+++ vendor/display/6.6/display-vendor.mk
@@ -0,0 +1,62 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/common/vendor/display/6.6
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/clstc_config_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/clstc_config_library.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU1010.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU1010.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU10__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU10__.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU11__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU11__.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU1230.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU1230.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU1240.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU1240.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU12__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU12__.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/DPU9__.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/DPU9__.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/sdm_display_resolution_extn.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/sdm_display_resolution_extn.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/display/thermallevel_to_fps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/thermallevel_to_fps.xml \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/init/feature_enabler_client.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/feature_enabler_client.rc \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/init/qdcmss.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/qdcmss.rc \
+    vendor/qcom/common/vendor/display/6.6/proprietary/vendor/etc/init/vendor.qti.hardware.display.color-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.display.color-service.rc
+
+PRODUCT_PACKAGES += \
+    mapper.qti \
+    libbacklight-calib \
+    libclstc_algorithm_adapter \
+    libclstc_gcp_adapter \
+    libclstc_tm \
+    libdigital-dimming \
+    libdisp-aba \
+    libdisplayqos \
+    libdisplayskuutils \
+    libdpps \
+    libfeutils \
+    libgame_enhance \
+    libhdr_backlight_adapter \
+    libhdr_tm \
+    libintervmipc \
+    libmemutils \
+    libmm-hdcpmgr \
+    libqdcm-algo \
+    libqdcm-json-mode-parser \
+    libqdcm-mode-parser \
+    libqrtrclient \
+    libqseed3 \
+    librcmask \
+    libsdedrm \
+    libsdm-color \
+    libsdm-colormgr-algo \
+    libsdm-disp-vndapis \
+    libsdmextension \
+    libsnapdragoncolor-manager \
+    libsnapdragoncolor-qdcm \
+    libtestutils \
+    libtinyxml2_1 \
+    libvmfilexfer \
+    mapper.qti.xml \
+    feature_enabler_client \
+    vendor.qti.hardware.display.color-service \
+    ppd \
+    qdcmss \
+    ubwcconvert
